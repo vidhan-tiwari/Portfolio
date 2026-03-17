1943c4dd1f2609887247d509bb8d51a98054ffe3
vidhan-tiwari <2k23csaiml2313975@gmail.com>
2026-03-17 14:40:06 +0530

    init: vite + react + tailwind project scaffold

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Hello,Building my portfolio!
+      </h1>
+    </div>
+  )
+}
+
+export default App
