86b40959d66948b8a7559c5e4731821ed2fbdb7f
vidhan-tiwari <2k23csaiml2313975@gmail.com>
2026-03-21 23:44:41 +0530

    Added dynamic text in Hero.jsx section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,56 @@
+import { useState, useEffect } from "react"
+
+const dynamicWords = [
+    "Machine Learning",
+    "Deep Learning",
+    "Data Science",
+    "Building Systems",
+]
+
 function Hero() {
+    const [wordIndex, setWordIndex] = useState(0)
+    const [text, setText] = useState("")
+    const [isDeleting, setIsDeleting] = useState(false)
+
+    useEffect(() => {
+        const currentWord = dynamicWords[wordIndex]
+
+        let timeout
+
+        if (!isDeleting && text === currentWord) {
+            // word completed -> pause before deleting
+            timeout = setTimeout(() => setIsDeleting(true), 1300)
+        } else if (!isDeleting) {
+            // Typing
+            timeout = setTimeout(() => {
+                setText(currentWord.substring(0, text.length + 1))
+            }, 50)
+        } else if (isDeleting && text.length === 0) {
+            // Deletion completed ->next word
+            setIsDeleting(false)
+            setWordIndex((prev) => (prev + 1) % dynamicWords.length)
+
+        } else {
+            // Deleting
+            timeout = setTimeout(() => {
+                setText(currentWord.substring(0, text.length - 1))
+            }, 50)
+        }
+
+        return () => clearTimeout(timeout)
+    }, [text, isDeleting, wordIndex])
+
     return (
         <section className="flex flex-col items-center justify-center text-center py-32 px-6">
             <p className="text-blue-400 font-medium mb-3 tracking-wide uppercase text-sm">Portfolio</p>
             <h1 className="text-5xl md:text-6xl font-bold mb-4">Hi, I'm Vidhan Tiwari</h1>
             <p className="text-lg md:text-xl text-gray-400 mb-8 max-w-2xl leading-relaxed">
-                Aspiring Software/ML Engineer passionate about Machine Learning, Data Science, and building interactive intelligent systems.            </p>
+                Aspiring Software / ML Engineer passionate about{" "}
+                <span className="text-blue-400 font-semibold">
+                    {text}
+                    <span className="animate-pulse">|</span>
+                </span>
+            </p>
             <a
                 href="#projects"
                 className="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg font-semibold transition-colors"
